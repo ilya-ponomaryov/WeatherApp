c22cf0487145f26be44b2df07e0c62ac56f02721
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-18 18:04:52 +0300

    ViewModel getWeatherFromNetwork added

--- app/src/main/java/com/example/weatherapp/di/DomainModule.kt
+++ app/src/main/java/com/example/weatherapp/di/DomainModule.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.di
 import com.example.weatherapp.domain.WeatherRepository
 import com.example.weatherapp.domain.usecases.GetWeatherFromNetworkUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
@@ -10,7 +11,7 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(ViewModelComponent::class)
 class DomainModule {
-
+    @Provides
     fun provideGetWeatherFromNetworkUseCase(weatherRepository: WeatherRepository)
     = GetWeatherFromNetworkUseCase(weatherRepository)
 }
--- app/src/main/java/com/example/weatherapp/ui/GeneralViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/GeneralViewModel.kt
@@ -1,9 +1,54 @@
 package com.example.weatherapp.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.Result
+import com.example.weatherapp.data.models.WeatherData
+import com.example.weatherapp.data.models.WeatherRequest
+import com.example.weatherapp.domain.usecases.GetWeatherFromNetworkUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class GeneralViewModel : ViewModel() {
+class GeneralViewModel @Inject constructor(
+    private val getWeatherFromNetworkUseCase: GetWeatherFromNetworkUseCase
+) : ViewModel() {
+    init {
+        getWeatherFromNetwork(WeatherRequest(
+            52.731118499999994,
+            41.434199146086904,
+            "minutely,alerts",
+            "metric",
+            "b4ffacf20b03eabce37d537a7d83a675"
+        ))
+    }
+
+    private val _weatherLiveData = MutableLiveData<WeatherData>()
+    val weatherLiveData : LiveData<WeatherData>
+    get() = _weatherLiveData
+
+    private fun getWeatherFromNetwork(weatherRequest: WeatherRequest) {
+
+
+        viewModelScope.launch {
+            when(val weatherResult = getWeatherFromNetworkUseCase.invoke(weatherRequest)) {
+                is Result.Loading -> {
+                    Log.d("ViewModelLog", "Loading")
+                }
+                is Result.Success -> {
+                    Log.d("ViewModelLog", "Success")
+                    _weatherLiveData.postValue(weatherResult.data!!)
+                    Log.d("ViewModelLog", weatherResult.data.toString())
+                }
+                is Result.Error -> {
+                    Log.d("ViewModelLog", "Error: ${weatherResult.exception}")
+                }
+            }
+        }
+    }
 
 }
