feb108e846737598ea7af8374c33cc417727eb4a
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-18 16:16:57 +0300

    WeatherRepository weather methods added

--- app/src/main/java/com/example/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,4 +1,28 @@
 package com.example.weatherapp.data.repository
 
-class WeatherRepositoryImpl {
+import com.example.weatherapp.Result
+import com.example.weatherapp.data.WeatherRemoteDataSource
+import com.example.weatherapp.data.models.*
+import com.example.weatherapp.domain.WeatherRepository
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val weatherRemoteDataSource: WeatherRemoteDataSource
+) : WeatherRepository {
+
+    /*override suspend fun getCurrentWeather(): Result<Current> {
+
+    }
+
+    override suspend fun getDailyWeather(): Result<Daily> {
+
+    }
+
+    override suspend fun getHourlyWeather(): Result<Hourly> {
+
+    }*/
+
+    override suspend fun getWeather(weatherRequest: WeatherRequest): Result<WeatherData> {
+        return weatherRemoteDataSource.getWeather(weatherRequest = weatherRequest)
+    }
 }
--- app/src/main/java/com/example/weatherapp/domain/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/domain/WeatherRepository.kt
@@ -1,11 +1,15 @@
 package com.example.weatherapp.domain
 
-import com.example.weatherapp.data.models.Current
-import com.example.weatherapp.data.models.Daily
+import com.example.weatherapp.Result
+import com.example.weatherapp.data.models.*
 
 interface WeatherRepository {
 
-    suspend fun getCurrentWeather() : Result<Current>
+    /*suspend fun getCurrentWeather() : Result<Current>
 
     suspend fun getDailyWeather() : Result<Daily>
+
+    suspend fun getHourlyWeather() : Result<Hourly>*/
+
+    suspend fun getWeather(weatherRequest: WeatherRequest) : Result<WeatherData>
 }
