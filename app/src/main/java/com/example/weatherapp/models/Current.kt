88c2f411d2fd27091ed871e90fe23faeb3f704a7
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-18 13:02:28 +0300

    Weather data models added

---
+++ app/src/main/java/com/example/weatherapp/models/Current.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp.models
+
+data class Current(
+    val clouds: Int,
+    val dew_point: Double,
+    val dt: Int,
+    val feels_like: Double,
+    val humidity: Int,
+    val pressure: Int,
+    val snow: Snow,
+    val sunrise: Int,
+    val sunset: Int,
+    val temp: Double,
+    val uvi: Double,
+    val visibility: Int,
+    val weather: List<WeatherX>,
+    val wind_deg: Int,
+    val wind_gust: Double,
+    val wind_speed: Double
+)
---
+++ app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.models
+
+data class Weather(
+    val alerts: List<Alert>,
+    val current: Current,
+    val hourly: List<Hourly>,
+    val lat: Double,
+    val lon: Double,
+    val minutely: List<Minutely>,
+    val timezone: String,
+    val timezone_offset: Int
+)
