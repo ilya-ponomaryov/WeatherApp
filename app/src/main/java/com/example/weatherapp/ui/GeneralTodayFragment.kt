7f039b1cf3b7366ad94a70afdb2f5cef7e3baf9f
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-18 18:34:33 +0300

    GeneralToday components added

---
+++ app/src/main/java/com/example/weatherapp/ui/GeneralTodayFragment.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+
+class GeneralTodayFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GeneralTodayFragment()
+    }
+
+    private lateinit var viewModel: GeneralTodayViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.general_today_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GeneralTodayViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/weatherapp/ui/GeneralTodayViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class GeneralTodayViewModel : ViewModel() {
+
+}
