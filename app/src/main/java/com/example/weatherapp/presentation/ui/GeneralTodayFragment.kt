8c2715ed5fb001c461790f6d4b3133614e84c37e
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-21 20:32:38 +0300

    TodayCard added, binding added

--- app/src/main/java/com/example/weatherapp/presentation/ui/GeneralTodayFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/ui/GeneralTodayFragment.kt
@@ -1,32 +1,38 @@
 package com.example.weatherapp.presentation.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.GeneralTodayFragmentBinding
 
 class GeneralTodayFragment : Fragment() {
+    private val viewModel: GeneralTodayViewModel by viewModels()
+    private var _binding: GeneralTodayFragmentBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = GeneralTodayFragment()
     }
 
-    private lateinit var viewModel: GeneralTodayViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.general_today_fragment, container, false)
+        _binding = GeneralTodayFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(GeneralTodayViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
