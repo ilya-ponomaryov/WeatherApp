4e7fe15aab8abb5a66f9f7879526c5b92099a29f
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-22 15:55:40 +0300

    AddCityDialogLayout keyboard show auto

--- app/src/main/java/com/example/weatherapp/presentation/ui/AddCityDialog.kt
+++ app/src/main/java/com/example/weatherapp/presentation/ui/AddCityDialog.kt
@@ -1,10 +1,7 @@
 package com.example.weatherapp.presentation.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
 import androidx.fragment.app.DialogFragment
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.AddCityDialogLayoutBinding
@@ -20,6 +17,10 @@ class AddCityDialog : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = AddCityDialogLayoutBinding.inflate(inflater, container, false)
+        binding.searchAddCityView.setIconifiedByDefault(true)
+        binding.searchAddCityView.isFocusable = true
+        binding.searchAddCityView.isIconified = false
+        binding.searchAddCityView.requestFocusFromTouch()
         binding.cancelAddCityBtn.setOnClickListener {
             dismiss()
         }
@@ -36,6 +37,10 @@ class AddCityDialog : DialogFragment() {
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.WRAP_CONTENT
         )
+        dialog?.window?.setGravity(Gravity.BOTTOM)
         dialog?.window?.setBackgroundDrawableResource(R.drawable.insert_dialog)
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
     }
+
+
 }
