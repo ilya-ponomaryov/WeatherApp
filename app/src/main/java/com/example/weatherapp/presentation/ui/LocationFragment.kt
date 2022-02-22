e59cdec2dd7aea9e9fdcb278f5a808d3438b6ff6
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-02-22 12:00:18 +0300

    LocationFragment viewBinding added

--- app/src/main/java/com/example/weatherapp/presentation/ui/LocationFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/ui/LocationFragment.kt
@@ -6,26 +6,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.GeneralFragmentBinding
+import com.example.weatherapp.databinding.LocationFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LocationFragment : Fragment() {
-
+    private val viewModel: LocationViewModel by viewModels()
+    private var _binding: LocationFragmentBinding? = null
+    private val binding get() = _binding!!
     companion object {
         fun newInstance() = LocationFragment()
     }
 
-    private lateinit var viewModel: LocationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.location_fragment, container, false)
+        _binding = LocationFragmentBinding.inflate(inflater, container, false)
+        binding.addCityLocationBtn.setOnClickListener {
+
+        }
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
