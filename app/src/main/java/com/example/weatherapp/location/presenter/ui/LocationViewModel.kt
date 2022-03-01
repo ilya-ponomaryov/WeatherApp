a7b4bd26ef51c437b96f44938957881d05154538
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-03-01 16:22:54 +0300

    Created getLocation method
    Added liveData for locationRequest

--- app/src/main/java/com/example/weatherapp/location/presenter/ui/LocationViewModel.kt
+++ app/src/main/java/com/example/weatherapp/location/presenter/ui/LocationViewModel.kt
@@ -1,21 +1,48 @@
 package com.example.weatherapp.location.presenter.ui
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.common.Result
 import com.example.weatherapp.location.data.models.Location
+import com.example.weatherapp.location.data.models.LocationRequest
 import com.example.weatherapp.location.domain.usecases.GetLocationFromNetworkUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LocationViewModel @Inject constructor(
     private val getLocationFromNetworkUseCase: GetLocationFromNetworkUseCase
 ) : ViewModel() {
+    private val TAG = "CityVM"
 
     private val _locationLiveData = MutableLiveData<Location>()
     val locationLiveData: LiveData<Location>
     get() = _locationLiveData
 
+    private val _locationRequestLiveData = MutableLiveData<LocationRequest>()
+    val locationRequestLiveData: LiveData<LocationRequest>
+        get() = _locationRequestLiveData
 
+
+    fun getLocationFromNetwork() {
+        viewModelScope.launch {
+            when (val locationResult = getLocationFromNetworkUseCase
+                .invoke(LocationRequest("Moscow", 1, "b4ffacf20b03eabce37d537a7d83a675"))) {
+                is Result.Success ->{
+                    Log.d(TAG, "getLocationFromNetwork: Success")
+                    _locationLiveData.value = locationResult.data!!
+                }
+                is Result.Loading -> {
+                    Log.d(TAG, "getLocationFromNetwork: Loading")
+                }
+                is Result.Error -> {
+                    Log.d(TAG, "getLocationFromNetwork: Error, ${locationResult.exception}")
+                }
+            }
+        }
+    }
 }
