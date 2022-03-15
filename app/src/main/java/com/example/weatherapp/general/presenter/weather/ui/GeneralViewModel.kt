df000584fe5dcf9ef2aaab875db573f814c085cc
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-03-15 17:21:26 +0300

    Rename getWeatherFromNetwork

--- app/src/main/java/com/example/weatherapp/general/presenter/weather/ui/GeneralViewModel.kt
+++ app/src/main/java/com/example/weatherapp/general/presenter/weather/ui/GeneralViewModel.kt
@@ -5,8 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.common.utils.ExceptionCatcher
-import com.example.weatherapp.general.data.weather.models.Weather
-import com.example.weatherapp.general.data.weather.models.WeatherAndLocation
 import com.example.weatherapp.general.data.weather.models.WeatherData
 import com.example.weatherapp.general.domain.usecases.weather.GetWeatherFromNetwork
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GeneralViewModel @Inject constructor(
-    private val getWeatherFromNetwork: GetWeatherFromNetwork
+    private val getWeather: GetWeatherFromNetwork
 ) : ViewModel() {
     private val _weather = MutableLiveData<WeatherData>()
     val weather: LiveData<WeatherData>
@@ -31,7 +29,7 @@ class GeneralViewModel @Inject constructor(
 
     fun loadWeather(city: String?) = viewModelScope.launch {
         try {
-            val result = getWeatherFromNetwork(city)
+            val result = getWeather(city)
             _weather.value = result.weatherData
             _city.value = result.location[0].local_names.ru
         } catch (e: Exception) {
