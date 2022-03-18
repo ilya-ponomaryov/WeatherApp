ff4eae4ca4cb7cfffd9edd55b0068fab961dcc2c
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-03-18 13:10:41 +0300

    Revert "Made WeatherModule methods as factory method"
    This reverts commit df89ff8b

--- app/src/main/java/com/example/weatherapp/general/data/weather/network/WeatherService.kt
+++ app/src/main/java/com/example/weatherapp/general/data/weather/network/WeatherService.kt
@@ -2,19 +2,17 @@ package com.example.weatherapp.general.data.weather.network
 
 import com.example.weatherapp.general.data.weather.models.WeatherData
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
     @GET("/data/2.5/onecall?")
     suspend fun getWeather(
-        @Query("lat") lat: Double,
-        @Query("lon") lon: Double,
-        @Query("exclude") exclude: String,
-        @Query("units") units: String,
-        @Query("lang") lang: String,
-        @Query("appid") appid: String,
+        @Query("lat")lat: Double,
+        @Query("lon")lon: Double,
+        @Query("exclude")exclude: String,
+        @Query("units")units: String,
+        @Query("lang")lang: String,
+        @Query("appid")appid: String,
     ): Response<WeatherData>
 }
