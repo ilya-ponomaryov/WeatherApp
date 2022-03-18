1515cca90ae121da8d52b7b99a214a4dd2e7af92
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-03-18 14:26:02 +0300

    Removed unused import
    Formatted code

--- app/src/main/java/com/example/weatherapp/general/presenter/weather/ui/GeneralFragment.kt
+++ app/src/main/java/com/example/weatherapp/general/presenter/weather/ui/GeneralFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.example.weatherapp.common.utils.showToast
 import com.example.weatherapp.databinding.GeneralFragmentBinding
 import com.example.weatherapp.general.presenter.weather.adapters.GeneralRvAdapter
 import com.example.weatherapp.general.presenter.location.ui.AddCityDialog
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class GeneralFragment : Fragment() {
