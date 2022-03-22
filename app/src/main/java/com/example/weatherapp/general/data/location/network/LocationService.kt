2e2e48278402e084b03069e1206ab12146639f71
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-03-22 14:19:58 +0300

    Formatted LocationService code

--- app/src/main/java/com/example/weatherapp/general/data/location/network/LocationService.kt
+++ app/src/main/java/com/example/weatherapp/general/data/location/network/LocationService.kt
@@ -5,11 +5,11 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
 interface LocationService {
     @GET("/geo/1.0/direct?")
     suspend fun getCity(
-        @Query("q")q: String,
-        @Query("limit")limit: Int,
-        @Query("appid")appid: String) : Response<Location>
+        @Query("q") q: String,
+        @Query("limit") limit: Int,
+        @Query("appid") appid: String
+    ): Response<Location>
 }
