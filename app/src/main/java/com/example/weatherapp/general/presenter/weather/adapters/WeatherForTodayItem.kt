8b9c773daa28c035998d31e1cde6568fa5bffd09
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-04-07 18:12:58 +0300

    Renamed weather_for_today.xml

--- app/src/main/java/com/example/weatherapp/general/presenter/weather/adapters/WeatherForTodayItem.kt
+++ app/src/main/java/com/example/weatherapp/general/presenter/weather/adapters/WeatherForTodayItem.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.example.weatherapp.R
-import com.example.weatherapp.databinding.TodayCardLayoutBinding
+import com.example.weatherapp.databinding.WeatherForTodayBinding
 import com.example.weatherapp.general.data.weather.models.WeatherForToday
 import com.mikepenz.fastadapter.binding.AbstractBindingItem
 
 
 open class WeatherForTodayItem(private val today: WeatherForToday) :
-    AbstractBindingItem<TodayCardLayoutBinding>() {
+    AbstractBindingItem<WeatherForTodayBinding>() {
     override var identifier: Long
         get() = today.hashCode().toLong()
         set(value) {}
@@ -21,9 +21,9 @@ open class WeatherForTodayItem(private val today: WeatherForToday) :
     override fun createBinding(
         inflater: LayoutInflater,
         parent: ViewGroup?
-    ): TodayCardLayoutBinding = TodayCardLayoutBinding.inflate(inflater, parent, false)
+    ) = WeatherForTodayBinding.inflate(inflater, parent, false)
 
-    override fun bindView(binding: TodayCardLayoutBinding, payloads: List<Any>) {
+    override fun bindView(binding: WeatherForTodayBinding, payloads: List<Any>) {
         binding.dateTodayCardText.text = today.date
         binding.tempTodayCardText.text = today.temperature
         binding.descTodayCardText.text = today.weatherDescription
@@ -33,7 +33,7 @@ open class WeatherForTodayItem(private val today: WeatherForToday) :
             .into(binding.iconTodayCardImg)
     }
 
-    override fun unbindView(binding: TodayCardLayoutBinding) {
+    override fun unbindView(binding: WeatherForTodayBinding) {
         binding.dateTodayCardText.text = null
         binding.tempTodayCardText.text = null
         binding.descTodayCardText.text = null
