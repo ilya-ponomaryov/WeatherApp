7518e22fcee4d56449f8267ff6bfeb4d8e9e5554
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-05-10 19:59:35 +0300

    Removed unused import

--- app/src/main/java/com/example/weatherapp/general/data/location/network/repository/LocationRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/general/data/location/network/repository/LocationRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.example.weatherapp.general.data.location.network.repository
 
-import com.example.weatherapp.common.utils.Constant
 import com.example.weatherapp.general.data.location.models.Location
 import com.example.weatherapp.general.data.location.network.LocationService
 import com.example.weatherapp.general.domain.LocationRepository
