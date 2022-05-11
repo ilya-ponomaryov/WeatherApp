360bcfabdd637821bf3b47d507c3b9ac8cbc592f
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-05-11 15:30:04 +0300

    Simplified methods, removed lambda

--- app/src/main/java/com/example/weatherapp/general/data/weather/network/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/general/data/weather/network/repository/WeatherRepositoryImpl.kt
@@ -12,18 +12,15 @@ import javax.inject.Inject
 
 class WeatherRepositoryImpl @Inject constructor(private val service: WeatherService) :
     WeatherRepository {
-    override fun getWeather(latitude: Double, longitude: Double): Single<Weather> {
-        return service.getWeather(
-            latitude,
-            longitude,
-        ).map { weatherData -> convertToWeather(weatherData)}
+    override fun getWeather(latitude: Double, longitude: Double) =
+        service.getWeather(latitude, longitude)
+            .map(::convertToWeather)
             .subscribeOn(Schedulers.io())
-    }
 
-    private fun convertToWeather(weatherData: WeatherData): Weather {
-        return Weather(
+    private fun convertToWeather(weatherData: WeatherData) =
+        Weather(
             toTodayEquipped(weatherData.current),
             toDailyEquippedList(weatherData.daily, weatherData.hourly)
         )
-    }
+
 }
