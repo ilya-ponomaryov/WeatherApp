27dd7fe4abe1ba8b14b0a0c3efade0e49eba9718
ilya-2000 <i.ponomaryov.00@gmail.com>
2022-05-16 18:41:21 +0300

    Redesigned code sequence, renamed WeatherGetterImpl.kt file

--- app/src/main/java/com/example/weatherapp/general/usecases/weather/WeatherGetter.kt
+++ app/src/main/java/com/example/weatherapp/general/usecases/weather/WeatherGetter.kt
@@ -6,6 +6,10 @@ import com.example.weatherapp.general.usecases.weather.models.WeatherAndCity
 import io.reactivex.Single
 import javax.inject.Inject
 
+interface WeatherGetter {
+    operator fun invoke(city: String): Single<WeatherAndCity>
+}
+
 class WeatherGetterImpl @Inject constructor(
     private val weatherService: WeatherRepository,
     private val locationService: LocationRepository
@@ -20,14 +24,10 @@ class WeatherGetterImpl @Inject constructor(
 
 }
 
-interface WeatherGetter {
-    operator fun invoke(city: String): Single<WeatherAndCity>
+interface WeatherRepository {
+    fun getWeather(latitude: Double, longitude: Double): Single<Weather>
 }
 
 interface LocationRepository {
     fun getLocation(cityName: String): Single<City>
-}
-
-interface WeatherRepository {
-    fun getWeather(latitude: Double, longitude: Double): Single<Weather>
 }
